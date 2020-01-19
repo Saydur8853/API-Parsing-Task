8821cd14e2b5bb3f47496f48699a5d46ca2cc36c
Saydur <saydur@Saydurs-Mac.local>
2020-01-19 14:09:42 -0800

    summary object view

--- API_callByCocoapod/ViewController.swift
+++ API_callByCocoapod/ViewController.swift
@@ -18,20 +18,57 @@ class ViewController: UIViewController {
         
         //Function Call for respose API & print them in log
         
-        Alamofire.request("https://api.darksky.net/forecast/688555c90fac582a0fbc3be8e444802b/37.8267,-122.4233").responseJSON{ response in
-            print(response.request as Any)
-            print(response.response as Any)
-            print(response.result.value as Any)
+        Alamofire.request("https://api.darksky.net/forecast/688555c90fac582a0fbc3be8e444802b/37.8267,-122.4233").responseJSON( completionHandler:{(response) in
+//            print(response.request as Any)
+//            print(response.response as Any)
+            //print(response.description)
+            //print(response.result.value as Any)
+            
             
             // Code for showing data in text view
-            self.text_view.text = response.description
-        }
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+            //self.text_view.text = response.description
+            var temp : Double = 0.0
+            if let jsonDictionary = response.result.value as? [String : Any] {
+                if let currentWeatherDictionary = jsonDictionary["currently"] as? [String : Any]{
+                    let summary = currentWeatherDictionary["summary"] as? String
+                    temp = currentWeatherDictionary["temperature"] as? Double ?? 0.0
+                   
+                    //Below code for converting double type too string 
+                    // self.text_view.text = String(temp)
+                    self.text_view.text = summary
+                    
+                    print(currentWeatherDictionary)
+                }
+            }
+//            print(jsonDictionary)
+        })
+}
+    
+//    func getCurrentWeather(completion: @escaping (CurrentWeather?) -> void){
+//        Alamofire.request("https://api.darksky.net/forecast/688555c90fac582a0fbc3be8e444802b/37.8267,-122.4233").responseJSON( completionHandler:{(response) in
+//            //            print(response.request as Any)
+//            //            print(response.response as Any)
+//            //print(response.description)
+//            //print(response.result.value as Any)
+//
+//
+//            // Code for showing data in text view
+//            //self.text_view.text = response.description
+//
+//            if let jsonDictionary = response.result.value as? [String : Any] {
+//                if let currentWeatherDictionary = jsonDictionary["currently"] as? [String : Any]{
+//                    //                    let summary = currentWeatherDictionary["pressure"] as? [String : Any]
+//                    //                    print(summary)
+//                    print(currentWeatherDictionary)
+//                    let currentWeather = CurrentWeather(weatherDictionary:currentWeatherDictionary)
+//                    completion(currentWeather)
+//                }
+//                else{
+//                    completion(nil)
+//                }
+//            }
+//            //            print(jsonDictionary)
+//        })
+//    }
 }
 
---
+++ Podfile
@@ -0,0 +1,12 @@
+
+# platform :ios, '9.0'
+
+target 'API_callByCocoapod' do
+
+  use_frameworks!
+
+pod 'Alamofire', '4.4.0'
+
+
+
+end
